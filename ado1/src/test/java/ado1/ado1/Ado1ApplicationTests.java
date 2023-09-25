0735f004e27d9a9a59b56898fda871d250a91689
Vinicius Souza <91327153+Vinicius-Souza-Araujo@users.noreply.github.com>
2023-09-25 12:09:26 -0300

    Add files via upload

---
+++ ado1/src/test/java/ado1/ado1/Ado1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ado1.ado1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ado1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
