fc125c4ffb1c0f543cb9f46930f3aceeccc92c98
Vinicius Souza <91327153+Vinicius-Souza-Araujo@users.noreply.github.com>
2023-09-25 21:30:43 -0300

    Add files via upload

---
+++ ado1/src/main/java/ado1/Ado1Application.java
@@ -0,0 +1,16 @@
+package ado1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Ado1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ado1Application.class, args);
+	}
+
+
+
+}
